19b9054a45211cdb53739aa402c76081a3d4ef52
Richard Davis <rdavis.bacs@gmail.com>
2016-06-20 13:22:06 -0400

    serve xml

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::AlbumsController < ApplicationController
   def index
     albums = Album.order(created_at: :desc).limit(10)
-    render json: albums
+
+    respond_to do |format|
+      format.json { render json: albums }
+      format.xml { render xml: albums }
+    end
   end
 end
--- spec/controllers/api/v1/albums_controller_spec.rb
+++ spec/controllers/api/v1/albums_controller_spec.rb
@@ -2,12 +2,23 @@ require "rails_helper"
 
 RSpec.describe Api::V1::AlbumsController, type: :controller do
   describe "#index" do
+    let!(:albums) { create_list(:album, 5) }
+
     it "returns an index of albums in the JSON format" do
-      albums = create_list(:album, 5)
-      get :index
+      get :index, format: :json
       json = JSON.parse(response.body)
+
       expect(json).to_not be_empty
       expect(json.first["title"]).to eq(albums.last.title)
+    end
+
+    it "returns an index of albums in the XML format" do
+      get :index, format: :xml
+      xml = Nokogiri::XML(response.body)
+      title = xml.xpath("//albums/album/title").first.text
+
+      expect(xml.errors).to be_empty
+      expect(title).to eq(albums.last.title)
     end
   end
 end
